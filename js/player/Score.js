f3a5be1982b5e6ce26dfa2f2a27b5eb0e23d3021
unknown <1209256521@qq.com>
2020-05-07 20:55:32 +0800

    first commit

---
+++ js/player/Score.js
@@ -0,0 +1,20 @@
+import {DataStore} from "../base/DataStore.js";
+
+export class Score{
+    constructor() {
+        this.ctx = DataStore.getInstance().ctx;
+        this.scoreNumber = 0;
+        this.isScore = true;
+    }
+
+    draw(){
+        this.ctx.font = '25px Arial';
+        this.ctx.fillStyle = 'rgba(1,36,170,0.39)';
+        this.ctx.fillText(
+            this.scoreNumber,
+            DataStore.getInstance().canvas.width / 10,
+            DataStore.getInstance().canvas.height / 18,
+            1000
+        )
+    }
+}
